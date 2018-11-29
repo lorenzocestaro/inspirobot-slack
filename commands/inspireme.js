292fcb0c7e34a8fd7a3411171626214e597ab08f
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2018-11-29 16:04:00 +0100

    Update .gitignote and https use

--- commands/inspireme.js
+++ commands/inspireme.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const http = require('http')
+const https = require('https')
 
 function getImageUrl() {
     return new Promise((resolve, reject) => {
         const url = 'http://inspirobot.me/api?generate=true';
         let image_url = '';
-        http.get(url, (res) => {
+        https.get(url, (res) => {
             res.on('data', (chunk) => { image_url = image_url + chunk });
             res.on('end', () => resolve(image_url));
         }).on("error", reject);
