b61efa6fb94a7873ecb2ddf96c4c278bdf7a9a83
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2018-12-05 16:34:09 +0100

    Refactor router with a routing table object

--- src/router.js
+++ src/router.js
@@ -3,21 +3,32 @@
 const inspirobot = require('./inspirobot');
 const slack = require('./slack');
 
-const router = async (request, response) => {
-  const { method, url } = request;
-
-  try {
-    if (method === 'GET' && url === '/quote') {
+const routingTable = {
+  quote: {
+    GET: async ({ response }) => {
       const quoteUrl = await inspirobot.generateQuote();
+      response.setHeader('Content-Type', 'text/plain');
       response.write(quoteUrl);
-    } else if (method === 'POST' && url === '/quote') {
+    },
+    POST: async ({ response }) => {
       const quoteUrl = await inspirobot.generateQuote();
       const body = slack.formatQuoteMessage({ image_url: quoteUrl });
       response.setHeader('Content-Type', 'application/json');
       response.write(JSON.stringify(body));
-    } else {
-      response.write('Nothing to see here');
-    }
+    },
+  },
+  notFound: ({ response }) => response.write('Nothing to see here.'),
+};
+
+const router = async (request, response) => {
+  const { method, url } = request;
+  const route = url.slice(1);
+
+  try {
+    const handler =
+      (routingTable[route] && routingTable[route][method]) ||
+      routingTable.notFound;
+    await handler({ request, response });
   } catch (error) {
     console.error(error);
     response.status(500);
