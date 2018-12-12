15193c9d9438a45531babc4b498377d33df322c2
Shy Alter <puemos@users.noreply.github.com>
2018-12-12 22:17:32 +0100

    formatQuoteMessage return fast

--- src/slack.js
+++ src/slack.js
@@ -1,12 +1,12 @@
 function formatQuoteMessage({ imageUrl, text = 'Get inspired!' }) {
-    if (imageUrl) {
-        return {
-            attachments: [{ image_url: imageUrl }],
-            response_type: 'in_channel',
-            text,
-        };
+    if (!imageUrl){
+        throw new TypeError('No image_url specified');
     }
-    throw new TypeError('No image_url specified');
+    return {
+        attachments: [{ image_url: imageUrl }],
+        response_type: 'in_channel',
+        text,
+    };
 }
 
 module.exports = {
