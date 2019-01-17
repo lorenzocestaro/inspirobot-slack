9b4e8628e0dda016a49e145f2ca419001325f779
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2019-01-17 11:43:13 +0100

    Change default port to 80

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 http.createServer(app).listen(PORT, () => {
     console.log(`server start at port ${PORT}`);
